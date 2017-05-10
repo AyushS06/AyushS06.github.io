59c090afa1e505ba2036752458d2e06a85e1d65a
AyushS06 <ayushs217@gmail.com>
2017-05-10 01:10:45 -0400

    Update site.js

--- site.js
+++ site.js
@@ -5,22 +5,19 @@ app.controller('instantSearchCtrl',function($scope,$http){
     // https://images-api.nasa.gov/search?q=apollo%2011&description=moon%20landing&media_type=image
     // data.json
     //alert('In controller');
-    $http.get('https://images-api.nasa.gov/search?q=apollo%2011&description=moon%20landing&media_type=image').success(function(data, status, headers, config) {
-        $scope.items = data.data;
-    }).error(function(data, status, headers, config) {
-        console.log("No data found..");
-  });
-});
-
-$scope.search = function($scope,$http) {
+    //$http.get('https://images-api.nasa.gov/search?q=apollo%2011&description=moon%20landing&media_type=image').success(function(data, status, headers, config) {
+    //    $scope.items = data.data;
+    //}).error(function(data, status, headers, config) {
+    //    console.log("No data found..");
+    //});
+    $scope.search = function() {
     alert('In controller');
     $http.get('https://images-api.nasa.gov/search?q=apollo%2011&description=moon%20landing&media_type=image').success(function(data, status, headers, config) {
         $scope.items = data.data;
     }).error(function(data, status, headers, config) {
         console.log("No data found..");
     });
-    //$scope.searchResult = calculateService.calculate($scope.quantity, 10);
-  };
+});
 
 app.filter('searchFor', function(){
     return function(arr, searchString){
