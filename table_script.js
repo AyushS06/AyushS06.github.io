41bd493543e371f8ece69b278af6f9c9284db403
Ayush Sharma <ayushs217@gmail.com>
2019-01-29 14:14:01 -0500

    Update table_script.js

--- table_script.js
+++ table_script.js
@@ -6,14 +6,23 @@ function edit_row(no)
  var name=document.getElementById("name_row"+no);
  var country=document.getElementById("country_row"+no);
  var age=document.getElementById("age_row"+no); 
+ var food=document.getElementById("food_row"+no);
+ var time=document.getElementById("time_row"+no);
+ var number=document.getElementById("number_row"+no);
 	
  var name_data=name.innerHTML;
  var country_data=country.innerHTML;
  var age_data=age.innerHTML;
+ var food_data=food.innerHTML;
+ var time_data=time.innerHTML;
+ var number_data=number.innerHTML;
 	
  name.innerHTML="<input type='text' id='name_text"+no+"' value='"+name_data+"'>";
  country.innerHTML="<input type='text' id='country_text"+no+"' value='"+country_data+"'>";
  age.innerHTML="<input type='text' id='age_text"+no+"' value='"+age_data+"'>";
+ food.innerHTML="<input type='text' id='food_text"+no+"' value='"+food_data+"'>";
+ time.innerHTML="<input type='text' id='time_text"+no+"' value='"+time_data+"'>";
+ number.innerHTML="<input type='text' id='number_text"+no+"' value='"+number_data+"'>";
 }
 
 function save_row(no)
@@ -21,10 +30,16 @@ function save_row(no)
  var name_val=document.getElementById("name_text"+no).value;
  var country_val=document.getElementById("country_text"+no).value;
  var age_val=document.getElementById("age_text"+no).value;
+ var food_val=document.getElementById("food_text"+no).value;
+ var time_val=document.getElementById("time_text"+no).value;
+ var number_val=document.getElementById("number_text"+no).value;
 
  document.getElementById("name_row"+no).innerHTML=name_val;
  document.getElementById("country_row"+no).innerHTML=country_val;
  document.getElementById("age_row"+no).innerHTML=age_val;
+ document.getElementById("food_row"+no).innerHTML=food_val;
+ document.getElementById("time_row"+no).innerHTML=time_val;
+ document.getElementById("number_row"+no).innerHTML=number_val;
 
  document.getElementById("edit_button"+no).style.display="block";
  document.getElementById("save_button"+no).style.display="none";
@@ -40,12 +55,18 @@ function add_row()
  var new_name=document.getElementById("new_name").value;
  var new_country=document.getElementById("new_country").value;
  var new_age=document.getElementById("new_age").value;
+ var new_food=document.getElementById("new_food").value;
+ var new_time=document.getElementById("new_time").value;
+ var new_number=document.getElementById("new_number").value;
 	
  var table=document.getElementById("data_table");
  var table_len=(table.rows.length)-1;
  var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'><td id='name_row"+table_len+"'>"+new_name+"</td><td id='country_row"+table_len+"'>"+new_country+"</td><td id='age_row"+table_len+"'>"+new_age+"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
-
+ var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'><td id='name_row"+table_len+"'>"+new_name+"</td><td id='country_row"+table_len+"'>"+new_country+"</td><td id='age_row"+table_len+"'>"+new_age+"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
  document.getElementById("new_name").value="";
  document.getElementById("new_country").value="";
  document.getElementById("new_age").value="";
+ document.getElementById("new_food").value="";
+ document.getElementById("new_time").value="";
+ document.getElementById("new_number").value="";
 }
